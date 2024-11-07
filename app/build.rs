8b3b65b89a80245f6975362d995bce95d90564c0
Christoffer SOOP <christoffer.soop@gmail.com>
2024-11-07 07:13:50 +0100

    removed example app mdoule that is replaced by relm module

--- app/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    glib_build_tools::compile_resources(
-        &["libindigo-rs-example-app/resources"],
-        "libindigo-rs-example-app/resources/resources.gresource.xml",
-        "libindigo-rs-example-app.gresource",
-    );
-}
