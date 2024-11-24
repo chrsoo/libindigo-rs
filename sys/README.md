3abbab422d15ab1b17c938f08826cf5e496e0eab
Christoffer SOOP <christoffer.soop@gmail.com>
2024-11-25 00:52:10 +0100

    adding support for INDIGO_SOURCE envar, submodule, and system libraries

--- sys/README.md
+++ sys/README.md
@@ -7,3 +7,15 @@ The `libindigo-sys` crate is a [bindgen](https://rust-lang.github.io/rust-bindge
 For more information on using the INDIGO system API, please refer to [INDIGO For Developers](https://www.indigo-astronomy.org/for-developers.html).
 
 The [NOTES](NOTES.md) document contains some raw notes on how to compile and set the development environment for  `libindigo-sys`. At some point these raw notes should be upgraded to proper documentation.
+
+The `libindigo-sys` crate will do one of the following in order of priority when building the crate
+
+1. Attempt to build INDIGO from the path of the envar INDIGO_SOURCE, if specified.
+1. Attempt to build INDIGO from the path of the git submodule `externals/indigo`, if it has been initialised by git.
+
+    ```bash
+    git submodule update --init --recursive
+    ```
+
+1. Use INDIGO system headers in `/usr/include` and libs from `/usr/lib`, if `/usr/include/indigo/indigo_version.h` exists.
+1. Initialise the git `externals/indigo` git submodule with the command above and build it.
--- sys/build.rs
+++ sys/build.rs
@@ -1,8 +1,8 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::{self, File};
+use std::io::{prelude::*, Result};
 use std::env;
 use std::io::BufReader;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use rev_buf_reader::RevBufReader;
 use semver::Version;
 use regex::Regex;
@@ -13,14 +13,10 @@ fn join_paths(base: &PathBuf, path: &str) -> String {
     String::from(s)
 }
 
-// TODO find all .c source files in dir
-fn _get_sources(src_dir: &PathBuf) -> Vec<PathBuf> {
-    vec![src_dir.join("indigo_bus.c")]
-}
-
 /// Compile a c source file to a binary object file.
-fn _compile(c_file: &PathBuf, include_path: &PathBuf) -> PathBuf {
-    let mut o_file = c_file.clone();
+fn compile(c_file: PathBuf, include_path: &PathBuf) -> PathBuf {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut o_file = out_path.join("indigo").join(c_file.file_name().unwrap());
     o_file.set_extension("o");
 
     let output = std::process::Command::new("clang")
@@ -33,9 +29,9 @@ fn _compile(c_file: &PathBuf, include_path: &PathBuf) -> PathBuf {
         .output()
         .expect("could not spawn `clang`");
 
-    println!("{}", String::from_utf8(output.stderr).unwrap());
-    println!();
     println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!();
+    println!("{}", String::from_utf8(output.stderr).unwrap());
 
     if !output.status.success() {
         panic!("could not compile object file");
@@ -44,7 +40,9 @@ fn _compile(c_file: &PathBuf, include_path: &PathBuf) -> PathBuf {
 }
 
 /// Parse the INDIGO Makefile and extract version and build numbers.
-fn parse_indigo_version<'a>(indigo_root: &PathBuf) -> std::io::Result<Version> {
+fn _parse_indigo_version<'a>(indigo_root: &PathBuf) -> std::io::Result<Version> {
+    // TODO extract build from indigo_config.h - what about version?
+    // let m = indigo_root.join("indigo_libs/indigo/indigo_config.h");
     let m = indigo_root.join("Makefile");
 
     let mut version = Version::new(0, 0, 0);
@@ -69,8 +67,8 @@ fn parse_indigo_version<'a>(indigo_root: &PathBuf) -> std::io::Result<Version> {
 }
 
 /// Ensure that the Cargo package version string includes the correct INDIGO build number.
-fn ensure_build_version(indigo_root: &PathBuf) -> std::io::Result<()>{
-    let indigo_version = parse_indigo_version(indigo_root)?;
+fn _ensure_build_version(indigo_root: &PathBuf) -> std::io::Result<()>{
+    let indigo_version = _parse_indigo_version(indigo_root)?;
 
     if let Ok(v) = env::var("CARGO_PKG_VERSION") {
         if let Ok(pkg_version) = Version::parse(&v) {
@@ -83,7 +81,7 @@ fn ensure_build_version(indigo_root: &PathBuf) -> std::io::Result<()>{
     Ok(())
 }
 
-fn taillog(file: &str, limit: usize, err: bool) {
+fn _taillog(file: &str, limit: usize, err: bool) {
     // let file = root.join(file);
     let file = File::open(file).unwrap();
 
@@ -97,52 +95,41 @@ fn taillog(file: &str, limit: usize, err: bool) {
 }
 
 fn main() -> std::io::Result<()> {
-    let indigo_root: PathBuf = if env::var("INDIGO_ROOT").is_ok() {
-        PathBuf::from(env::var("INDIGO_ROOT").expect("path defined by INDIGO_ROOT envar not found"))
-    } else {
-        PathBuf::from("externals/indigo")
+
+    let include = if let Ok(envar) = env::var("INDIGO_SOURCE") { // build from source dir
+        let indigo_source = PathBuf::from(envar);
+        let indigo_source = indigo_source.canonicalize().expect("cannot canonicalize path");
+        build_indigo(&indigo_source)?
+    } else if let Ok(submodule) = Path::new("externals/indigo").canonicalize() { // build from submodule
+        build_indigo(&submodule)?
+    } else if Path::new("/usr/include/indigo/indigo_version.h").is_file() { // use system libraries
+        let include_dir = PathBuf::from("/usr/include");
+
+        let lib_dir = Path::new("/usr/lib");
+        output_cargo_link_search(&lib_dir);
+        let libindigo = lib_dir.join("libindigo.a");
+        println!("cargo:rustc-link-lib={}", libindigo.to_str().expect("could not find /usr/lib/libindigo.a"));
+
+        include_dir
+    } else { // last ditch effort, checkout and build submodule
+        let outcome = std::process::Command::new("git")
+            .arg("submodule")
+            .arg("update")
+            .arg("--init")
+            .arg("--recursive")
+            // .current_dir(&indigo_root.join("indigo_libs"))
+            // .stdout(stdin)
+            // .stderr(stderr)
+            .status()
+            .expect("could not spawn `make`")
+            ;
+        if !outcome.success() {
+            panic!("could not checkout externals/indigo");
+        }
+
+        let submodule = Path::new("externals/indigo").canonicalize()?;
+        build_indigo(&submodule)?
     };
-    let indigo_root = indigo_root.canonicalize().expect("cannot canonicalize path");
-    ensure_build_version(&indigo_root)?;
-
-    // lib source directory in `indigo/indigo_libs`
-    let indigo_libs: PathBuf = indigo_root.join("indigo_libs");
-
-    let log = File::create("libindigo-sys.log").unwrap();
-    let err = File::create("libindigo-sys.err").unwrap();
-    let stdin = std::process::Stdio::from(log);
-    let stderr = std::process::Stdio::from(err);
-
-    // run make and write to build.out - panic if it fails
-    let status = std::process::Command::new("make")
-        .arg("all")
-        .current_dir(&indigo_root)
-        .stdout(stdin)
-        .stderr(stderr)
-        .status()
-        .expect("could not spawn `make`");
-
-    if !status.success() {
-        println!("libindigo-sys.log:\n...");
-        taillog("libindigo-sys.log", 10, false);
-        println!("---");
-        eprintln!("libindigo-sys.err:\n...");
-        taillog("libindigo-sys.err", 10, true);
-        eprintln!("---");
-        panic!("could not make {}", indigo_root.to_str().expect("indigo root not found"));
-    }
-
-    let indigo_build = indigo_root.join("build");
-    let indigo_build_libs = indigo_build.join("lib");
-
-    // Tell cargo to look for shared (system) libraries in `indigo/build/libs`
-    println!("cargo:rustc-link-search={}", indigo_build_libs.to_str().expect("path not found"));
-
-    // search the `indigo/build/libs` for native (static) librarires
-    println!("cargo::rustc-link-search=native={}", indigo_build_libs.to_str().expect("path not found"));
-
-    // bind with the `libindigo.a` static library in the `indigo/build/libs` directory
-    println!("cargo:rustc-link-lib=indigo");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -150,14 +137,14 @@ fn main() -> std::io::Result<()> {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .clang_arg(format!("-I{}", indigo_libs.to_str().expect("path not found")))
-        .header(join_paths(&indigo_libs, "indigo/indigo_names.h"))
-        .header(join_paths(&indigo_libs, "indigo/indigo_bus.h"))
-        .header(join_paths(&indigo_libs, "indigo/indigo_client.h"))
-        .header(join_paths(&indigo_libs, "indigo/indigo_driver.h"))
-        .header(join_paths(&indigo_libs, "indigo/indigo_config.h"))
-        .header(join_paths(&indigo_libs, "indigo/indigo_timer.h"))
-        .header(join_paths(&indigo_libs, "indigo/indigo_token.h"))
+        .clang_arg(format!("-I{}", include.to_str().expect("path not found")))
+        .header(join_paths(&include, "indigo/indigo_names.h"))
+        .header(join_paths(&include, "indigo/indigo_bus.h"))
+        .header(join_paths(&include, "indigo/indigo_client.h"))
+        .header(join_paths(&include, "indigo/indigo_driver.h"))
+        .header(join_paths(&include, "indigo/indigo_config.h"))
+        .header(join_paths(&include, "indigo/indigo_timer.h"))
+        .header(join_paths(&include, "indigo/indigo_token.h"))
         .no_copy(".*")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
@@ -172,6 +159,69 @@ fn main() -> std::io::Result<()> {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    Ok(())
+}
+
+// Tell cargo to look for shared (system) libraries in `/usr/lib`
+// search the `${indigo_root}/indigo/build/libs` for native (static) librarires
+fn output_cargo_link_search(lib_dir: &Path) {
+    let lib_dir = lib_dir.to_str().expect("could not find library dir");
+    println!("cargo:rustc-link-search={}", lib_dir);
+    println!("cargo:rustc-link-search=native={}", lib_dir);
+}
+
+/// compile source in rust
+fn build_indigo(indigo_root: &PathBuf) -> Result<PathBuf> {
+    // ensure_build_version(&indigo_root)?;
+
+    let source_dir = indigo_root.join("indigo_libs");
+
+    fs::read_dir(&source_dir)?
+        .into_iter()
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap().path())
+        .filter(|p| p.is_file() && p.ends_with(".c"))
+        .map(|c| compile(c, &source_dir))
+        .for_each(drop) // noop
+        ;
+
+    let indigo_libs = source_dir.join("indigo_libs");
+    output_cargo_link_search(&indigo_libs);
+
+    let libindgo = source_dir.join("build/lib/libindigo.a");
+    println!("cargo:rustc-link-lib={}", libindgo.to_str().expect("could not find build/lib/libindigo.a"));
+
+    Ok(indigo_libs)
+}
+
+/// compile source code using external make
+fn _make_indigo(indigo_root: &PathBuf) -> Result<()>{
+    _ensure_build_version(&indigo_root)?;
+
+    let log = File::create("libindigo-sys.log").unwrap();
+    let err = File::create("libindigo-sys.err").unwrap();
+    let stdin = std::process::Stdio::from(log);
+    let stderr = std::process::Stdio::from(err);
+
+    // run make and write to build.out - panic if it fails
+    let status = std::process::Command::new("make")
+        .arg("all")
+        .current_dir(&indigo_root.join("indigo_libs"))
+        .stdout(stdin)
+        .stderr(stderr)
+        .status()
+        .expect("could not spawn `make`");
+
+    if !status.success() {
+        println!("libindigo-sys.log:\n...");
+        _taillog("libindigo-sys.log", 10, false);
+        println!("---");
+        eprintln!("libindigo-sys.err:\n...");
+        _taillog("libindigo-sys.err", 10, true);
+        eprintln!("---");
+        panic!("could not make {}", indigo_root.to_str().expect("indigo root not found"));
+    }
 
     Ok(())
 }
